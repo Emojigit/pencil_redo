b794bfb120f28073bd59d498a83f2ff6a7ea9be8
Emoji <55009343+Emojigit@users.noreply.github.com>
2023-01-25 14:07:22 +0800

    NEW PENCIL!

--- init.lua
+++ init.lua
@@ -1,84 +1,78 @@
--- Minetest Mod: pencil_redo - text_table.lua
--- (c) 2020 Emoji
+-- Minetest Mod: pencil_redo - init.lua
+-- (c) 2020, 2022  Emoji
 -- Under LGPLv2.1
 
-local modname = minetest.get_current_modname()
-local path = minetest.get_modpath(modname)
-local default_here = minetest.get_modpath("default")
-
-local function check_protection(pos, name, text)
-	if minetest.is_protected(pos, name) then
-		minetest.log("action", (name ~= "" and name or "A mod")
-			.. " tried to " .. text
-			.. " at protected position "
-			.. minetest.pos_to_string(pos))
-		minetest.record_protection_violation(pos, name)
-		return true
-	end
-	return false
-end
-
+local path = minetest.get_modpath("pencil_redo")
+local S = minetest.get_translator("pencil_redo")
+local gui = flow.widgets
 pencil_redo = {}
 
-dofile(path.."/text_table.lua")
+-- We no longer need a table
+minetest.register_alias("pencil_redo:table","air")
+
+local my_gui = flow.make_gui(function(player, ctx)
+	return gui.VBox {
+		gui.Label {
+			label = S("Change the infotext of node at @1",minetest.pos_to_string(ctx.pos)),
+		},
+		gui.Box{w = 1, h = 0.05, color = "grey", padding = 0.25},
+		gui.Textarea {
+			name = "infotextField",
+			h = 3, w = 6, default = minetest.get_meta(ctx.pos):get_string("infotext")
+		},
+		gui.HBox {
+			gui.ButtonExit {
+				label = S("Abort"), w = 1, expand = true,
+			},
+			gui.ButtonExit {
+				label = S("Confirm"), w = 1, expand = true,
+				on_event = function(player,ctx)
+					local pos = ctx.pos
+					local name = player:get_player_name()
+					if minetest.is_protected(name,pos) then
+						minetest.record_protection_violation(name,pos)
+						return
+					end
+					local meta = minetest.get_meta(ctx.pos)
+					if ctx.form.infotextField then
+						minetest.log("action",name .. "used a pencil on node at " .. minetest.pos_to_string(pos) .. " with the following infotext:\n" .. ctx.form.infotextField)
+						meta:set_string("infotext",ctx.form.infotextField)
+					end
+				end
+			},
+		}
+	}
+end)
 
 minetest.register_craftitem("pencil_redo:pencil", {
-	description = "Pencil",
-	groups = {flammable = 2},
-	on_use = function(itemstack, user, pointed_thing)
-		local pmeta = itemstack:get_meta()
-		local text = pmeta:get_string("ptext") or "" 
-		if not(pointed_thing.type == "node") then
+	description = S("Pencil"),
+	_doc_items_longdesc = S("Tool to edit infotext of nodes"),
+	_doc_items_usagehelp = S("Rightclick on a node to edit its infotext."),
+	on_place = function(itemstack, placer, pointed_thing)
+		if not placer:is_player() then return end
+		local pos = pointed_thing.under
+		local node = minetest.get_node(pos).name
+		local name = placer:get_player_name()
+		if node == "air" or node == "ignore" then
+			minetest.chat_send_player(name,S("Invalid node!"))
+			return
+		elseif minetest.is_protected(name,pos) then
+			minetest.record_protection_violation(name,pos)
 			return
 		end
-		local pos = minetest.get_pointed_thing_position(pointed_thing, above)
-		if check_protection(pos, user and user:get_player_name() or "", " Used a pencil with text "..text.." .") then
-			return
-		end
-		local meta = minetest.get_meta(pos);
-		meta:set_string("infotext",text);
-		local log_front = ""
-		if user:is_player() then
-			log_front = "Player "..user:get_player_name()
-		else
-			log_front = "A non-player Object"
-		end
-		minetest.log("action",log_front.." Used a pencil with text "..text.." .")
+		my_gui:show(placer,{pos=pos})
 	end,
-	inventory_image = "pencil.png"
+	inventory_image = "pencil_pencil.png",
+	stack_max = 1,
 })
 
-if default_here then
+if default then
 	minetest.register_craft({
-		output = "pencil_redo:pencil 4",
+		output = "pencil_redo:pencil",
 		recipe = {
-			{"default:coal_lump", "", ""},
-			{"group:stick", "", ""},
-			{"group:stick", "", ""},
-		},
-	})
-	minetest.register_craft({
-		output = "pencil_redo:pencil 4",
-		recipe = {
-			{"", "default:coal_lump", ""},
-			{"", "group:stick", ""},
-			{"", "group:stick", ""},
-		},
-	})
-	minetest.register_craft({
-		output = "pencil_redo:pencil 4",
-		recipe = {
-			{"", "", "default:coal_lump"},
-			{"", "", "group:stick"},
-			{"", "", "group:stick"},
-		},
-	})
-	minetest.register_craft({
-		output = "pencil_redo:table",
-		recipe = {
-			{"group:wood", "default:cobble", "group:wood"},
-			{"group:wood", "default:diamond", "group:wood"},
-			{"group:wood", "group:wood", "group:wood"},
+			{"default:coal_lump"},
+			{"group:stick"},
+			{"group:stick"},
 		},
 	})
 end
@@ -88,22 +82,3 @@ minetest.register_craft({
 	recipe = "pencil_redo:pencil",
 	burntime = 5,
 })
-minetest.register_craft({
-	type = "fuel",
-	recipe = "pencil_redo:table",
-	burntime = 10,
-})
-
-pencil_redo.get_pencil_stack = function(text)
-	local stack = ItemStack("pencil:pencil 1")
-	local meta = stack:get_meta()
-	local stext = tostring(text)
-	if stext=="" or type(text) == "nil" then
-		meta:set_string("ptext", "")
-		meta:set_string("description", "Pencil")
-	else
-		meta:set_string("ptext", text)
-		meta:set_string("description", "Pencil With Text")
-	end
-	return stack
-end
